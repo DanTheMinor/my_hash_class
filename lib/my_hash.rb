b8113b5421def8199b8d2c43c0f23da02692ba99
Adam McFadden & Dan Minor <student@epicodus.com>
2015-04-27 10:07:39 -0700

    add a new spec to return true if the hash contains a particular key. Pass both specs.

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -1,16 +1,34 @@
 class MyHash
   define_method(:initialize) do
+    #@hash = [@key, @value]
     @key = []
     @value = []
   end
   #MyHash.new('1', 'red' => '2', 'blue')
   #MyHash.store(key, value)
   define_method(:store) do |key, value|
-    @key = key
-    @value = value
+    @key.push(key)
+    @value.push(value)
   end
 
   define_method(:fetch) do |key|
-    @value
+    index = 0
+    until @key.length() == index
+      if @key[index] == key
+        return @value[index]
+      end
+      index += 1
+    end
+  end
+
+  define_method(:has_key?) do |key|
+    index = 0
+    until @key.length() == index
+      if @key[index] == key
+        return true
+      end
+      index += 1
+    end
+    return false
   end
 end
--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -1,12 +1,19 @@
 require('rspec')
 require('my_hash')
+require('pry')
 
 describe(MyHash) do
-  describe('#fetch') do
-    it('retieves a stored value by its key') do
+  describe('MyHash#fetch') do
+    it('retrieves a stored value by its key') do
       test_hash = MyHash.new()
       test_hash.store('kitten', 'cute')
       expect(test_hash.fetch('kitten')).to(eq('cute'))
     end
+  end
+  describe('MyHash#has_key?')
+    it('returns true if the key exists within the hash') do
+      test_hash = MyHash.new()
+      test_hash.store('red', 'blue')
+      expect(test_hash.has_key?('red')).to(eq(true))
   end
 end
